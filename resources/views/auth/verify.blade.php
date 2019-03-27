e19ece171138d1bae7ed988433611a653911b43e
vagrant <vagrant@homestead>
2019-03-27 09:06:21 +0000

    邮箱验证功能

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,17 +5,17 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">验证您的邮箱地址</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            一个新的验证邮箱的电子邮件已经发送到你的邮箱
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+                    在继续访问之前，请检查您的电子邮件以获取验证连接，
+                    如果你没有收到验证邮箱的电子邮件， <a href="{{ route('verification.resend') }}">请点击此处重新发送</a>.
                 </div>
             </div>
         </div>
