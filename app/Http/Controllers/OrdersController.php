56655dfaa586206d3e17d49c11ae3f5315f9823f
vagrant <vagrant@homestead>
2019-03-30 03:13:00 +0000

    购物车和订单

---
+++ app/Http/Controllers/OrdersController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\OrderRequest;
+use App\Models\UserAddress;
+use App\Models\Order;
+use Illuminate\Http\Request;
+use App\Services\OrderService;
+
+class OrdersController extends Controller
+{
+    public function index(Request $request)
+    {
+        $orders = Order::query()
+            // 使用 with 方法预加载，避免N + 1问题
+            ->with(['items.product', 'items.productSku'])
+            ->where('user_id', $request->user()->id)
+            ->orderBy('created_at', 'desc')
+            ->paginate();
+
+        return view('orders.index', ['orders' => $orders]);
+    }
+
+    public function show(Order $order, Request $request)
+    {
+        $this->authorize('own', $order);
+        return view('orders.show', ['order' => $order->load(['items.productSku', 'items.product'])]);
+    }
+
+    public function store(OrderRequest $request, OrderService $orderService)
+    {
+        $user    = $request->user();
+        $address = UserAddress::find($request->input('address_id'));
+
+        return $orderService->store($user, $address, $request->input('remark'), $request->input('items'));
+    }
+}
